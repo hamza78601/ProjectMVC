5df1857a036e17e8545b9b785c1248a6b09f568d
asp <asp@Aptechnk.edu>
2018-09-25 20:22:48 +0500

    mvc project

---
+++ mvcOne/mvcOne/Controllers/Api/NewRentalController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using mvcOne.Models;
+using mvcOne.Dtos;
+using System.Data.Entity;
+namespace mvcOne.Controllers.Api
+{
+    public class NewRentalController : ApiController
+    {
+        private ApplicationDbContext _context;
+        [HttpPost]
+
+        public  IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            var customer = _context.Customer.Single(c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movie.Where(m => newRental.MoviesIds.Contains(m.Id));
+            
+            foreach(var movie in movies)
+            {
+                if(movie.NumberAvailable ==0)
+                    return BadRequest("Movie Is Not Available");
+                movie.NumberAvailable--;
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+                _context.Rental.Add(rental);
+            }
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
---
+++ mvcOne/mvcOne/Migrations/201809151511545_updatingMovieColumns.cs
@@ -0,0 +1,16 @@
+namespace mvcOne.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatingMovieColumns : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
